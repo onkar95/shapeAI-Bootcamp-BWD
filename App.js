46a7f4ba3b6721852943f64d9614b418138f4680
onkar95 <66904041+onkar95@users.noreply.github.com>
2021-06-06 13:19:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      
+      <div className="header">
+        <h1>shapeAPI </h1>
+      </div>
+      <div className="note">
+        <h1> javascript and react.js </h1>
+        <p>
+          this was an amazing bootcamp covered by shaurya sinha we covered
+          everything from scratch HTML, JAVASCRIPT REACT.JS
+        </p>
+      </div>
+      <div className="note">
+        <h1> javascript and react.js </h1>
+        <p>
+          this was an amazing bootcamp covered by shaurya sinha we covered
+          everything from scratch HTML, JAVASCRIPT REACT.JS
+        </p>
+      </div>
+      <div className="note">
+        <h1> javascript and react.js </h1>
+        <p>
+          this was an amazing bootcamp covered by shaurya sinha we covered
+          everything from scratch HTML, JAVASCRIPT REACT.JS
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
